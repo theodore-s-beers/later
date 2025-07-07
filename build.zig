ffc73229f8a1311169ac50dab8a2ae404fe9d339
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2025-07-07 14:19:49 -0400

    Start organizing code in modules

--- build.zig
+++ build.zig
@@ -14,7 +14,13 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    // lib_mod.addImport("foo", foo_mod);
+    const decode_mod = b.createModule(.{
+        .root_source_file = b.path("src/decode.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    lib_mod.addImport("decode", decode_mod);
 
     // Now, we will create a static library based on the module we created above.
     // This creates a `std.Build.Step.Compile`, which is the build step responsible
@@ -30,6 +36,10 @@ pub fn build(b: *std.Build) void {
     const lib_unit_tests = b.addTest(.{ .root_module = lib_mod });
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
+    const decode_unit_tests = b.addTest(.{ .root_module = decode_mod });
+    const run_decode_unit_tests = b.addRunArtifact(decode_unit_tests);
+
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
+    test_step.dependOn(&run_decode_unit_tests.step);
 }
--- src/root.zig
+++ src/root.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
-const testing = std.testing;
+
+const decode = @import("decode");
 
 const Table = enum { ducet, cldr };
 
@@ -49,10 +50,10 @@ const Collator = struct {
         self.a_chars.clearRetainingCapacity();
         self.b_chars.clearRetainingCapacity();
 
-        const a_codepoints = try bytesToCodepoints(self.alloc, a);
+        const a_codepoints = try decode.bytesToCodepoints(self.alloc, a);
         defer self.alloc.free(a_codepoints);
 
-        const b_codepoints = try bytesToCodepoints(self.alloc, b);
+        const b_codepoints = try decode.bytesToCodepoints(self.alloc, b);
         defer self.alloc.free(b_codepoints);
 
         try self.a_chars.appendSlice(a_codepoints);
@@ -69,67 +70,6 @@ const Collator = struct {
     }
 };
 
-pub fn bytesToCodepoints(alloc: std.mem.Allocator, input: []const u8) ![]u32 {
-    var codepoints = try std.ArrayList(u32).initCapacity(alloc, input.len);
-    errdefer codepoints.deinit();
-
-    var state: u8 = UTF8_ACCEPT;
-    var codepoint: u32 = 0;
-
-    for (input) |b| {
-        const new_state = decode(&state, &codepoint, b);
-
-        if (new_state == UTF8_REJECT) {
-            codepoints.appendAssumeCapacity(REPLACEMENT);
-            state = UTF8_ACCEPT;
-        } else if (new_state == UTF8_ACCEPT) {
-            codepoints.appendAssumeCapacity(codepoint);
-        }
-    }
-
-    // If we ended in an incomplete sequence, emit replacement
-    if (state != UTF8_ACCEPT) {
-        codepoints.appendAssumeCapacity(REPLACEMENT);
-    }
-
-    return codepoints.toOwnedSlice();
-}
-
-const UTF8_ACCEPT: u8 = 0;
-const UTF8_REJECT: u8 = 12;
-
-const REPLACEMENT: u32 = 0xFFFD;
-
-const utf8d = [_]u8{
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-    1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,  9,
-    7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,
-    8,  8,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,
-    10, 3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  4,  3,  3,  11, 6,  6,  6,  5,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,
-
-    0,  12, 24, 36, 60, 96, 84, 12, 12, 12, 48, 72, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 0,  12, 12, 12, 12, 12, 0,
-    12, 0,  12, 12, 12, 24, 12, 12, 12, 12, 12, 24, 12, 24, 12, 12, 12, 12, 12, 12, 12, 12, 12, 24, 12, 12, 12, 12, 12, 24, 12, 12,
-    12, 12, 12, 12, 12, 24, 12, 12, 12, 12, 12, 12, 12, 12, 12, 36, 12, 36, 12, 12, 12, 36, 12, 12, 12, 12, 12, 36, 12, 36, 12, 12,
-    12, 36, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12,
-};
-
-inline fn decode(state: *u8, cp: *u32, byte: u8) u8 {
-    const class = utf8d[byte];
-
-    cp.* = if (state.* != UTF8_ACCEPT)
-        (byte & 0x3F) | (cp.* << 6)
-    else
-        ((@as(u8, 0xFF) >> @truncate(class)) & byte);
-
-    const idx = @as(usize, 256) + state.* + class;
-    state.* = utf8d[idx];
-
-    return state.*;
-}
-
 test "collation of identical strings" {
     const allocator = std.testing.allocator;
     var collator = Collator.initDefault(allocator);
@@ -138,5 +78,5 @@ test "collation of identical strings" {
     const a = "مصطفى";
     const b = "مصطفى";
 
-    try testing.expect(collator.collate(a, b));
+    try std.testing.expect(collator.collate(a, b));
 }
