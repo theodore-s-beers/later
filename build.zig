de423b675a63cf2fc8741742d2a4e41a7ace4e15
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2025-07-09 16:06:17 -0400

    Make root module publicly available

--- build.zig
+++ build.zig
@@ -8,7 +8,7 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib_mod = b.createModule(.{
+    const lib_mod = b.addModule("later", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
